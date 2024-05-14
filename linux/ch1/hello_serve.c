2f3a2fb73932116dc765eb27ab37c0d740c38cd3
thz <505503675@qq.com>
2024-05-14 22:27:30 +0800

    modift

---
+++ linux/ch1/hello_serve.c
@@ -0,0 +1,22 @@
+/*
+1,socket
+2,bind
+3,listen
+4,accept
+# 基于linux 需要unistd.h 
+# 基于Windows
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+void error_handing(char *message);
+
+int main(int argc,char* argv[]){
+
+
+
+}
